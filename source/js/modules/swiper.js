e3022aa0c0ccad2f1f4f68c41f52a93605268110
MARIA POMOGAEVA <114220160+Maria-Pomgaeva@users.noreply.github.com>
2023-07-14 16:11:39 +0700

    Update swiper.js

--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -1,9 +1,5 @@
-// import swiper JS
-import Swiper, {Navigation} from 'swiper';
-
 export const initSwiper = function () {
   const swiperTrainers = new Swiper('.trainers__items', {
-    modules: [Navigation],
     navigation: {
       nextEl: '.trainers__swiper-btn-next',
       prevEl: '.trainers__swiper-btn-prev',
@@ -11,7 +7,6 @@ export const initSwiper = function () {
   });
 
   const swiperComment = new Swiper('.comment__items', {
-    modules: [Navigation],
     navigation: {
       nextEl: '.comment__swiper-btn-next',
       prevEl: '.comment__swiper-btn-prev',
