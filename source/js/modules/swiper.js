b2199106348b5c01f4b3341d3182a03283c3a24f
MARIA POMOGAEVA <114220160+Maria-Pomgaeva@users.noreply.github.com>
2023-07-14 15:29:23 +0700

    Update swiper.js

--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -1,12 +1,20 @@
-// // import swiper JS
-// import Swiper, {Navigation, Pagination} from 'swiper';
+// import swiper JS
+import Swiper, {Navigation} from 'swiper';
 
-// export const initSwiper = function () {
-//   const swiper = new Swiper('.trainers__swiper', {
-//     modules: [Navigation, Pagination],
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   });
-// };
+export const initSwiper = function () {
+  const swiperTrainers = new Swiper('.trainers__items', {
+    modules: [Navigation],
+    navigation: {
+      nextEl: '.trainers__swiper-btn-next',
+      prevEl: '.trainers__swiper-btn-prev',
+    },
+  });
+
+  const swiperComment = new Swiper('.comment__items', {
+    modules: [Navigation],
+    navigation: {
+      nextEl: '.comment__swiper-btn-next',
+      prevEl: '.comment__swiper-btn-prev',
+    },
+  });
+};
