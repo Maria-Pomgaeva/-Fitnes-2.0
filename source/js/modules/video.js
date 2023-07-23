4441bb2378cd6d38017aabc2b6ce4d5c96374909
Maria Pomogaeva <maria.pomogaeva1@gmail.com>
2023-07-23 22:14:09 +0700

    Доработка картинок

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -1,16 +1,15 @@
-let video = document.getElementById('player');
-let link = document.getElementById('link');
-
-// const video = document.querySelector('.about__video-player');
-// const link = document.querySelector('.about__button-video');
+const video = document.querySelector('.about__video-player');
+const link = document.querySelector('.about__button-video');
 
 export const videoLoad = () => {
-  link.addEventListener('click', () => {
-    if (video.classList.contains('active')) {
-      return;
-    }
-    let src = video.dataset.src;
-    video.classList.add('active');
-    video.insertAdjacentHTML('afterbegin', '<iframe src="' + src + '"  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
-  });
+  if (link && video) {
+    link.addEventListener('click', () => {
+      if (video.classList.contains('active')) {
+        return;
+      }
+      let src = video.dataset.src;
+      video.classList.add('active');
+      video.insertAdjacentHTML('afterbegin', '<iframe src="' + src + '"  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
+    });
+  }
 };
